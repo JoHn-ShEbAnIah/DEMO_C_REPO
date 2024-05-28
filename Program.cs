671c35cd1c08ddab6245136d6d7f6e9aaeef0517
Martha Leena <marthaleena@Marthas-MacBook-Air.local>
2024-05-28 16:07:47 +0530

    Adding a READ me FILE

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+    namespace Methods
+    {   
+        class cars 
+          {  public string model ;
+            public string color;
+            static void Main(string[] args)
+                {
+                    cars ford = new cars(); 
+                    ford.model = "mustang";
+                    ford.color = "black";
+                    Console.WriteLine(ford.model);
+                }
+           }
+        }
+    
+        
+
